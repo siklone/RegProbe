1cffc39d61e3f61a39322651af93f1b293a1ca38
siklone <barzhooo@gmail.com>
2026-04-16 22:17:29 +0300

    fix: restore app framework globals

--- app/GlobalUsings.Application.cs
+++ app/GlobalUsings.Application.cs
@@ -1,3 +1,7 @@
+global using System.IO;
+global using System.Net.Http;
+global using Application = System.Windows.Application;
+
 global using RegProbe.Application.Models;
 global using RegProbe.Application.Services;
 global using RegProbe.Application.Services.TweakProviders;
