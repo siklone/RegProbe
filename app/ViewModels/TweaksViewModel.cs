213ed0beda18f2680b4269625484f9e31bf7dc28
siklone <barzhooo@gmail.com>
2026-05-01 17:40:47 +0300

    Tighten workspace shell layout for compact windows

--- app/ViewModels/TweaksViewModel.cs
+++ app/ViewModels/TweaksViewModel.cs
@@ -409,8 +409,8 @@ public sealed class TweaksViewModel : ViewModelBase, IDisposable
     }
 
     public GridLength SecondaryPanelWidth => IsSecondaryPanelCollapsed
-        ? new GridLength(72)
-        : new GridLength(240);
+        ? new GridLength(68)
+        : new GridLength(224);
 
     public int SearchFocusRequestId
     {
